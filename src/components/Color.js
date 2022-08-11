e72a32ed4d97f9adb7765de83e950eadca2a3874
jonathans199 <jso1919@gmail.com>
2022-08-10 21:06:28 -0400

    updated repo with separated components

---
+++ src/components/Color.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+
+const ColorComp = () => {
+  const [textColor, setTextColor] = useState('Green')
+
+  // TextColor is our Getter (Const variable)
+  // SetTextColor is our Setter (function) that updates textColor
+
+  console.log('TextColor -> ', textColor)
+
+  return (
+    <div className='App'>
+      {/* only more JSX allowed */}
+      <header className='App-header'>
+        <h1 style={{ color: textColor }}>My favorite Color is {textColor}</h1>
+        <button onClick={() => setTextColor('blue')}>Blue</button>
+        <br />
+        <button onClick={() => setTextColor('red')}>Red</button>
+        <br />
+        <button onClick={() => setTextColor('green')}>Green</button>
+      </header>
+    </div>
+  )
+}
+
+export default ColorComp
---
+++ src/components/WithFragment.js
@@ -0,0 +1,13 @@
+const CompWithFragment = () => {
+  return (
+    <>
+      <h1>With Fragment</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus voluptatibus id magnam, molestiae nisi dicta cumque voluptatum nesciunt
+        explicabo aliquam, vitae at commodi debitis eos doloribus quasi quo tenetur quidem.
+      </p>
+    </>
+  )
+}
+
+export default CompWithFragment
